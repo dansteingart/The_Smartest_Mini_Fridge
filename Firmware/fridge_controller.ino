ead8f16214573208b5fe2b5e9fc4c1e5ca94652a
Dan Steingart <dan@pithy.io>
2018-07-31 21:28:27 -0400

    PID working for heat

--- Firmware/fridge_controller.ino
+++ Firmware/fridge_controller.ino
@@ -1,28 +1,55 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <RelayShield.h>
+#include <pid.h>
 
 RelayShield myRelays;
 char publishString[200]; //a place holer for the publish string
 int state = 0;
 
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,1,4,1, PID::DIRECT);
+int WindowSize = 1000;
+unsigned long windowStartTime;
+unsigned long sendTime;
+bool pidder = false;
+
 void setup() {
     //.begin() sets up a couple of things and is necessary to use the rest of the functions
     myRelays.begin();
     Particle.function("set_state", set_state);
-    
+    Particle.function("set_temp", set_temp);
+
+    //PID Stuff
+    windowStartTime = millis();
+
+    //initialize the variables we're linked to
+    Setpoint = -42;
+
+    //tell the PID to range between 0 and the full window size
+    myPID.SetOutputLimits(0, WindowSize);
+
+    //turn the PID on
+    myPID.SetMode(PID::AUTOMATIC);
+
+    //send Stuff
+    sendTime = millis();
+
 }
 
-void loop() 
+void loop()
 {
-    
+
     int a0 = analogRead(0);
     int a1 = analogRead(1);
     int a2 = analogRead(2);
     int a3 = analogRead(3);
     int a4 = analogRead(4);
-    
-    for (int i = 1; i < 50; i++) 
-    {   
+
+    for (int i = 1; i < 50; i++)
+    {
     a0 += analogRead(0);
     a1 += analogRead(1);
     a2 += analogRead(2);
@@ -36,11 +63,27 @@ void loop()
     a3 = a3/51;
     a4 = a4/51;
 
+    Input = a0;
+    myPID.Compute();
 
+    if(millis() - windowStartTime>WindowSize)
+    { //time to shift the Relay Window
+      windowStartTime += WindowSize;
+    }
 
-    sprintf(publishString,"{\"a0\": %d, \"a1\": %d, \"a2\": %d,\"a3\": %d, \"a4\": %d,\"state\":%d}",a0,a1,a2,a3,a4,state);
-    Particle.publish("coke_fridge_test",publishString);
-    delay(5000);
+    if (pidder)
+    {
+      if(Output > millis() - windowStartTime) stater(1); //fixed it?
+      else stater(0);
+    }
+
+    if (millis() - sendTime > 5000)
+    {
+      sprintf(publishString,"{\"a0\": %d, \"a1\": %d, \"a2\": %d,\"a3\": %d, \"a4\": %d,\"setpoint\":%f,\"output\":%f,\"state\":%d}",a0,a1,a2,a3,a4,Setpoint,Output,state);
+      Particle.publish("coke_fridge_test",publishString);
+      sendTime = millis();
+    }
+
 
 }
 
@@ -55,7 +98,7 @@ void hot()
     off();
     myRelays.on(1);
     myRelays.on(3);
-    
+
 }
 
 void cold()
@@ -63,19 +106,31 @@ void cold()
     off();
     myRelays.on(2);
     myRelays.on(4);
-    
+
 }
 
 
+void stater(int port)
+{
+  state = port;
+  if (port == 0) off();
+  else if (port == 1) hot();
+  else if (port == 2) cold();
+  else off();
+}
+
 int set_state(String potter)
 {
+  pidder = true;
+  stater(potter.toInt());
+  //break the input string down into two parts.
+  return potter.toInt();
+}
 
-//break the input string down into two parts.
-int port = potter.charAt(0)-48; //1 or 2
-state = port;
-if (port == 0) off();
-else if (port == 1) hot();
-else if (port == 2) cold();
-else off();
-return potter.toInt();
+int set_temp(String potter)
+{
+  pidder = true;
+  Setpoint = potter.toInt();
+
+  return Setpoint;
 }
